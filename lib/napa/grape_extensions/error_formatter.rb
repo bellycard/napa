8fb0d5b17d000adc9855dfb0fa3552000f44cbb1
Darby Frey <darbyfrey@gmail.com>
2013-12-10 15:47:30 -0600

    Added Grape error presenter

---
+++ lib/napa/grape_extensions/error_formatter.rb
@@ -0,0 +1,18 @@
+if defined?(Grape)
+  module Grape
+    module ErrorFormatter
+      module Json
+        class << self
+          def call(message, backtrace, options = {}, env = nil)
+            result = message.is_a?(Napa::JsonError) ? message : Napa::JsonError.new(:api_error, message)
+
+            if (options[:rescue_options] || {})[:backtrace] && backtrace && !backtrace.empty?
+              result = result.merge(backtrace: backtrace)
+            end
+            MultiJson.dump(result)
+          end
+        end
+      end
+    end
+  end
+end
