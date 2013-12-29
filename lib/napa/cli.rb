fe17ccf57f04eb33891075db1dace8f7118e45da
Ben Kimpel <ben.p.kimpel@gmail.com>
2013-12-29 15:45:06 -0600

    Made arguments consistent

--- lib/napa/cli.rb
+++ lib/napa/cli.rb
@@ -3,7 +3,7 @@ require 'napa/generators'
 
 module Napa
   class CLI < Thor
-    register(Generators::ScaffoldGenerator, 'new', 'new <name> [path]',
+    register(Generators::ScaffoldGenerator, 'new', 'new <app_name> [app_path]',
              'Create a new Napa scaffold')
   end
 end
