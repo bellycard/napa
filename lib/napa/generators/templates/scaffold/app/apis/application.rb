c604f8e33b0b93393085fc28f08ea8823363cfa0
Jay OConnor <jay@bellycard.com>
2014-02-18 15:08:19 -0600

    active record exception catching by default

--- lib/napa/generators/templates/scaffold/app/apis/application.rb
+++ lib/napa/generators/templates/scaffold/app/apis/application.rb
@@ -1,13 +1,11 @@
 module Application
   class API < Grape::API
     format :json
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      rack_response({ error: { code: :record_not_found, message: 'record not found' } }.to_json, 404)
-    end
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      rack_response({ error: { code: :record_invalid, message: e.message } }.to_json, 500)
-    end
+    extend Napa::ActiveRecordExceptionCatchers
+    handle_active_record_errors
+
     mount HelloApi => '/'
+
     add_swagger_documentation
   end
 end
