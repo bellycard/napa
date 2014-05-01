37d1ed8e08432a2ed2023f0044b78226a932673c
Darby Frey <darbyfrey@gmail.com>
2014-05-01 10:15:59 -0500

    set encodings in the generator

--- lib/napa/generators/scaffold_generator.rb
+++ lib/napa/generators/scaffold_generator.rb
@@ -15,9 +15,10 @@ module Napa
       def generate
         say 'Generating scaffold...'
 
-        @database_gem     = ['pg','postgres'].include?(options[:database]) ? 'pg' : 'mysql2'
-        @database_adapter = ['pg','postgres'].include?(options[:database]) ? 'postgresql' : 'mysql2'
-        @database_user    = ['pg','postgres'].include?(options[:database]) ? '' : 'root'
+        @database_gem       = ['pg','postgres'].include?(options[:database]) ? 'pg' : 'mysql2'
+        @database_adapter   = ['pg','postgres'].include?(options[:database]) ? 'postgresql' : 'mysql2'
+        @database_encoding  = ['pg','postgres'].include?(options[:database]) ? 'unicode' : 'utf8'
+        @database_user      = ['pg','postgres'].include?(options[:database]) ? '' : 'root'
 
         directory ".", (app_path || app_name)
 
