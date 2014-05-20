345ada5054bb9c8de0a9b3cd9d7358974aa7c775
Clark Kampfe <clark.kampfe@gmail.com>
2014-05-20 10:42:31 -0500

    silence STDOUT

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,25 @@
 require 'napa'
+
+# from https://gist.github.com/adamstegman/926858
+RSpec.configure do |config|
+  config.before(:all) { silence_output }
+  config.after(:all) { enable_output }
+end
+
+# Redirects stderr and stdout to /dev/null.
+def silence_output
+  @orig_stderr = $stderr
+  @orig_stdout = $stdout
+
+  # redirect stderr and stdout to /dev/null
+  $stderr = File.new('/dev/null', 'w')
+  $stdout = File.new('/dev/null', 'w')
+end
+
+# Replace stdout and stderr so anything else is output correctly.
+def enable_output
+  $stderr = @orig_stderr
+  $stdout = @orig_stdout
+  @orig_stderr = nil
+  @orig_stdout = nil
+end
