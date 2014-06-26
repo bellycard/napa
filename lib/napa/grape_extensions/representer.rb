c1e11f0dc8c80ef4bef9d07f2832345be9af9712
Jay OConnor <jay@bellycard.com>
2014-06-26 15:14:53 -0500

    include null keys

--- lib/napa/grape_extensions/representer.rb
+++ lib/napa/grape_extensions/representer.rb
@@ -6,6 +6,9 @@ module Napa
   class Representer < Roar::Decorator
     include Roar::Representer::JSON
     include Representable::Coercion
+    def self.property(name, options={}, &block)
+      super(name, options.merge(render_nil: true), &block)
+    end
 
     property :object_type, getter: lambda { |*| self.class.name.underscore }
   end
