be358518f424121dda6f701911676efafc3c11cd
Daniel Mackey <dmackey@bellycard.com>
2014-08-06 15:28:48 -0500

    Napa::StatsDTimer spec

---
+++ spec/stats_d_timer_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'napa/stats_d_timer'
+
+class FooTimer
+  include Napa::StatsDTimer
+end
+
+describe Napa::StatsDTimer do
+  before do
+    # Delete any prevous instantiations of the emitter
+    Napa::Stats.emitter = nil
+    # Stub out logging since there is no log to output to
+    Napa::Logger.stub_chain(:logger, :warn)
+  end
+
+  it 'logs a timing event based on how long the block takes' do
+    expect(Napa::Stats.emitter).to receive(:timing).with('foo', an_instance_of(Float))
+    FooTimer.report_time('foo') do
+      sleep(0.1)
+    end
+  end
+
+end
