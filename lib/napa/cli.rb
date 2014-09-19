bab34bb2bd56c0cd60074dfb761161e11ade99a1
Daniel Mackey <dmackey@bellycard.com>
2014-09-19 09:44:42 -0500

    Add prompt back in, but have option to confirm for 1-command deploy

--- lib/napa/cli.rb
+++ lib/napa/cli.rb
@@ -64,13 +64,15 @@ module Napa
       end
 
       desc 'deploy [target]', 'Deploys A Service to a given target (i.e. production, staging, etc.)'
-      method_options :force => :boolean, :revision => :string
+      method_options :force => :boolean, :revision => :string, :confirm => :boolean
       def deploy(environment)
-        deploy = Napa::Deploy.new(environment, force: options[:force], revision: options[:revision])
-        if deploy.deployable?
-          say(deploy.deploy!, Thor::Shell::Color::GREEN)
-        else
-          say("Deploy Failed:\n#{deploy.errors.join("\n")}", Thor::Shell::Color::RED)
+        if options[:confirm] || yes?('Are you sure you want to deploy this service?', Thor::Shell::Color::YELLOW)
+          deploy = Napa::Deploy.new(environment, force: options[:force], revision: options[:revision])
+          if deploy.deployable?
+            say(deploy.deploy!, Thor::Shell::Color::GREEN)
+          else
+            say("Deploy Failed:\n#{deploy.errors.join("\n")}", Thor::Shell::Color::RED)
+          end
         end
       end
 
