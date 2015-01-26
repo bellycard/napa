4c65c035152f1752be109ad80a177b1fa6227526
Jay OConnor <joconnor@lumoslabs.com>
2015-01-25 18:27:59 -0800

    update grape, ruby, roar gems

--- lib/napa/output_formatters/representer.rb
+++ lib/napa/output_formatters/representer.rb
@@ -1,11 +1,11 @@
+require 'roar/json'
 require 'roar/decorator'
-require 'roar/representer/json'
-require 'roar/representer/feature/coercion'
+require 'roar/coercion'
 
 module Napa
   class Representer < Roar::Decorator
-    include Roar::Representer::JSON
-    include ::Representable::Coercion
+    include Roar::JSON
+    include Roar::Coercion
 
     property :object_type, getter: lambda { |*| self.class.name.underscore }
   end
