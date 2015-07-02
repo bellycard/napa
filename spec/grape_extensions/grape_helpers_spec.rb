3cbe7a23676e6431b9ddc1891da29cb995b7d09d
Shak <shakerislam@gmail.com>
2015-07-02 15:36:45 -0500

    fix styling and removing unnecessary puts statements

--- spec/grape_extensions/grape_helpers_spec.rb
+++ spec/grape_extensions/grape_helpers_spec.rb
@@ -2,29 +2,23 @@ require 'spec_helper'
 require 'napa/grape_extensions/error_formatter'
 
 describe Napa::GrapeHelpers do
-    describe 'represent' do
-        it 'should not rely on to_a if the data is enumerable with :map' do
-            class Foo
-                def to_a
-                    puts "Called to_a"
-                end
-                def map
-                    puts "Called map"
-                end
-            end
+  describe '#represent' do
+    it 'should not rely on to_a if the data is enumerable with :map' do
+      class Foo
+      end
 
-            class FooRepresenter < Napa::Representer
-            end    
+      class FooRepresenter < Napa::Representer
+      end    
 
-            class DummyClass
-                include Napa::GrapeHelpers
-            end        
+      class DummyClass
+          include Napa::GrapeHelpers
+      end        
 
-            foo = double("Foo")
-            expect(foo).not_to receive(:to_a)
-            expect(foo).to receive(:map) { ["foo", "bar"] }
+      foo = double("Foo")
+      expect(foo).not_to receive(:to_a)
+      expect(foo).to receive(:map) { ['foo', 'bar'] }
 
-            DummyClass.new.represent(foo, with: FooRepresenter)
-        end
+      DummyClass.new.represent(foo, with: FooRepresenter)
     end
+  end
 end
