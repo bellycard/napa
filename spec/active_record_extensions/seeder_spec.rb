2b2ecac668b3a17b31228cd893a5ffc81b4880f0
Ozer Chagatai <ochagatai@enova.com>
2015-12-01 16:42:28 -0600

    fix for style violations in spec/active_record_extensions/seeder_spec.rb

--- spec/active_record_extensions/seeder_spec.rb
+++ spec/active_record_extensions/seeder_spec.rb
@@ -3,10 +3,9 @@ require 'spec_helper'
 require 'napa/active_record_extensions/seeder'
 
 describe Napa::ActiveRecordSeeder do
-
   context 'when a valid file is not provided' do
     it 'raises an exception' do
-    	expect{Napa::ActiveRecordSeeder.load_file 'not-a-file'}.to raise_error
+      expect { Napa::ActiveRecordSeeder.load_file 'not-a-file' }.to raise_error
     end
   end
 
