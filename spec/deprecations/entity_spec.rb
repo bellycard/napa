d12453992840fe5656a3cc6cdba9aad5b0921cca
Ozer Chagatai <ochagatai@enova.com>
2015-12-01 16:25:18 -0600

    fix for style violations in spec/deprecations/entity_spec.rb and spec/deprecations/filter_by_hash_spec.rb

--- spec/deprecations/entity_spec.rb
+++ spec/deprecations/entity_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'active_support'
 
-describe "Napa::Entity Deprecation" do
+describe 'Napa::Entity Deprecation' do
   it 'raises a deprecation warning when a class inherits' do
     expect(ActiveSupport::Deprecation).to receive(:warn)
     class FooEntity < Napa::Entity; end
--- spec/deprecations/filter_by_hash_spec.rb
+++ spec/deprecations/filter_by_hash_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Napa::FilterByHash Deprecation" do
+describe 'Napa::FilterByHash Deprecation' do
   it 'raises a deprecation warning when mixed in to a class' do
     expect(ActiveSupport::Deprecation).to receive(:warn)
     class FooModel; include Napa::FilterByHash; end
