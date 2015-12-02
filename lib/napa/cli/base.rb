25d5980fc693328a870b95172fedcc9978e2ed98
Ozer Chagatai <ochagatai@enova.com>
2015-12-01 19:42:53 -0600

    fix for style violations in lib/napa/cli/base.rb, lib/napa/cli/base/server.rb, lib/napa/cli/base/version.rb, lib/napa/cli/generate/api.rb, lib/napa/cli/generate/readme.rb

--- lib/napa/cli/base.rb
+++ lib/napa/cli/base.rb
@@ -1,17 +1,15 @@
-require "napa/cli/base/console"
-require "napa/cli/base/deploy"
-require "napa/cli/base/new"
-require "napa/cli/base/server"
-require "napa/cli/base/version"
-require "napa/cli/generate"
+require 'napa/cli/base/console'
+require 'napa/cli/base/deploy'
+require 'napa/cli/base/new'
+require 'napa/cli/base/server'
+require 'napa/cli/base/version'
+require 'napa/cli/generate'
 
 module Napa
   module CLI
     class Base < Thor
-
-      desc "generate [COMMAND]", "Generate new code"
-      subcommand "generate", Napa::CLI::Generate
-
+      desc 'generate [COMMAND]', 'Generate new code'
+      subcommand 'generate', Napa::CLI::Generate
     end
   end
 end
