3771d8a88b8e606b84bfa84cf1791df348a928c6
Ozer Chagatai <ochagatai@enova.com>
2015-12-01 19:48:41 -0600

    fix for style violations in lib/napa/cli/base/console.rb, lib/napa/cli/base/deploy.rb, lib/napa/cli/base/new.rb

--- lib/napa/cli/base/new.rb
+++ lib/napa/cli/base/new.rb
@@ -4,14 +4,13 @@ require 'active_support/core_ext/string'
 module Napa
   module CLI
     class Base < Thor
-
       include Thor::Actions
 
-      source_root File.expand_path("../../templates/new", __FILE__)
+      source_root File.expand_path('../../templates/new', __FILE__)
+      DESC = 'Preconfigure for selected database (options: mysql/postgres/pg)'
+      option :database, default: 'mysql', aliases: '-d', desc: DESC
 
-      option :database, default: 'mysql', aliases: '-d', desc: 'Preconfigure for selected database (options: mysql/postgres/pg)'
-
-      desc "new <NAME> [PATH]", "Create a new Napa application"
+      desc 'new <NAME> [PATH]', 'Create a new Napa application'
       def new(name, path = nil)
         say 'Generating scaffold...'
 
@@ -23,27 +22,22 @@ module Napa
         @database_encoding  = postgres? ? 'unicode'    : 'utf8'
         @database_user      = postgres? ? ''           : 'root'
 
-        directory ".", (path || name)
+        directory '.', (path || name)
 
         say 'Done!', :green
       end
 
       no_commands do
 
-        def app_name
-          @name
-        end
-
-        def app_path
-          @path
-        end
+        attr_reader :name, :path
+        alias_method :app_name, :name
+        alias_method :app_path, :path
 
         def postgres?
-          %w[pg postgres].include?(options[:database])
+          %w(pg postgres).include?(options[:database])
         end
 
       end
-
     end
   end
 end
