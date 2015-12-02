00c74c9b30bb960b60f0c8b950e89de55b03007e
Ozer Chagatai <ochagatai@enova.com>
2015-12-01 19:19:43 -0600

    fix for style violations in lib/napa/deprecations/active_support_behavior.rb, lib/napa/deprecations/application_api.rb, and lib/napa/deprecations/grape_entity.rb

--- lib/napa/deprecations/active_support_behavior.rb
+++ lib/napa/deprecations/active_support_behavior.rb
@@ -1,8 +1,8 @@
 if Napa.env.test?
   ActiveSupport::Deprecation.behavior = :stderr
 else
-  ActiveSupport::Deprecation.behavior = ->(message, callstack) {
+  ActiveSupport::Deprecation.behavior = lambda do |message, _callstack|
     # ignoring callstack for now because it's fairly noisy
     Napa::Logger.logger.info(napa_deprecation_warning: message)
-  }
+  end
 end
--- lib/napa/deprecations/application_api.rb
+++ lib/napa/deprecations/application_api.rb
@@ -1,8 +1,10 @@
 module Napa
   class Deprecations
+    LINK = 'https://github.com/bellycard/napa/blob/master/lib/napa/generators/templates/scaffold/app/apis/application_api.rb'
+    WARNING = "no application_api.rb file found in app/apis, see #{LINK} for an example"
     def self.application_api_check
       unless File.exists?('./app/apis/application_api.rb')
-        ActiveSupport::Deprecation.warn 'no application_api.rb file found in app/apis, see https://github.com/bellycard/napa/blob/master/lib/napa/generators/templates/scaffold/app/apis/application_api.rb for an example', caller
+        ActiveSupport::Deprecation.warn WARNING, caller
       end
     end
   end
