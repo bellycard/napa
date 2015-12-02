c33752ecbf2b8c2b6c70cf1a98160eaebec6872f
Ozer Chagatai <ochagatai@enova.com>
2015-12-01 19:38:21 -0600

    fix for style violations in lib/napa/cli/generate.rb, lib/napa/cli/generated_attribute.rb

--- lib/napa/cli/generated_attribute.rb
+++ lib/napa/cli/generated_attribute.rb
@@ -7,8 +7,8 @@ module Napa
       UNIQ_INDEX_OPTIONS = %w(uniq)
 
       attr_accessor :name, :type
-      attr_reader   :attr_options
-      attr_writer   :index_name
+      attr_reader :attr_options
+      attr_writer :index_name
 
       class << self
         def parse(column_definition)
@@ -18,7 +18,6 @@ module Napa
           # type should be set blank so GeneratedAttribute's constructor
           # could set it to :string
           has_index, type = type, nil if INDEX_OPTIONS.include?(type)
-
           type, attr_options = *parse_type_and_options(type)
           type = type.to_sym if type
 
@@ -36,23 +35,24 @@ module Napa
 
         private
 
-        # parse possible attribute options like :limit for string/text/binary/integer, :precision/:scale for decimals or :polymorphic for references/belongs_to
+        # parse possible attribute options like :limit for string/text/binary/integer
+        # :precision/:scale for decimals or :polymorphic for references/belongs_to
         # when declaring options curly brackets should be used
         def parse_type_and_options(type)
           case type
           when /(string|text|binary|integer)\{(\d+)\}/
-            return $1, limit: $2.to_i
+            return Regexp.last_match[1], limit: Regexp.last_match[2].to_i
           when /decimal\{(\d+)[,.-](\d+)\}/
-            return :decimal, precision: $1.to_i, scale: $2.to_i
+            return :decimal, precision: Regexp.last_match[1].to_i, scale: Regexp.last_match[2].to_i
           when /(references|belongs_to)\{polymorphic\}/
-            return $1, polymorphic: true
+            return Regexp.last_match[1], polymorphic: true
           else
             return type, {}
           end
         end
       end
 
-      def initialize(name, type=nil, index_type=false, attr_options={})
+      def initialize(name, type = nil, index_type = false, attr_options = {})
         @name           = name
         @type           = type || :string
         @has_index      = INDEX_OPTIONS.include?(index_type)
@@ -62,47 +62,47 @@ module Napa
 
       def field_type
         @field_type ||= case type
-          when :integer              then :number_field
-          when :float, :decimal      then :text_field
-          when :time                 then :time_select
-          when :datetime, :timestamp then :datetime_select
-          when :date                 then :date_select
-          when :text                 then :text_area
-          when :boolean              then :check_box
-          else
-            :text_field
+                        when :integer              then :number_field
+                        when :float, :decimal      then :text_field
+                        when :time                 then :time_select
+                        when :datetime, :timestamp then :datetime_select
+                        when :date                 then :date_select
+                        when :text                 then :text_area
+                        when :boolean              then :check_box
+                        else
+                          :text_field
         end
       end
 
       def default
         @default ||= case type
-          when :integer                     then 1
-          when :float                       then 1.5
-          when :decimal                     then "9.99"
-          when :datetime, :timestamp, :time then Time.now.to_s(:db)
-          when :date                        then Date.today.to_s(:db)
-          when :string                      then name == "type" ? "" : "MyString"
-          when :text                        then "MyText"
-          when :boolean                     then false
-          when :references, :belongs_to     then nil
-          else
-            ""
+                     when :integer                     then 1
+                     when :float                       then 1.5
+                     when :decimal                     then '9.99'
+                     when :datetime, :timestamp, :time then Time.now.to_s(:db)
+                     when :date                        then Date.today.to_s(:db)
+                     when :string                      then name == 'type' ? '' : 'MyString'
+                     when :text                        then 'MyText'
+                     when :boolean                     then false
+                     when :references, :belongs_to     then nil
+                     else
+                       ''
         end
       end
 
       def factory_stub
         case type
-          when :integer                     then "1"
-          when :float                       then "1.5"
-          when :decimal                     then "9.99"
-          when :datetime, :timestamp, :time then "{ Time.now }"
-          when :date                        then "{ Date.today }"
-          when :string                      then '"MyString"'
-          when :text                        then '"MyText"'
-          when :boolean                     then "false"
-          when :digest                      then '"password"'
-          else
-            '"Unknown"'
+        when :integer                     then '1'
+        when :float                       then '1.5'
+        when :decimal                     then '9.99'
+        when :datetime, :timestamp, :time then '{ Time.now }'
+        when :date                        then '{ Date.today }'
+        when :string                      then '"MyString"'
+        when :text                        then '"MyText"'
+        when :boolean                     then 'false'
+        when :digest                      then '"password"'
+        else
+          '"Unknown"'
         end
       end
 
@@ -120,10 +120,10 @@ module Napa
 
       def index_name
         @index_name ||= if polymorphic?
-          %w(id type).map { |t| "#{name}_#{t}" }
-        else
-          column_name
-        end
+                          %w(id type).map { |t| "#{name}_#{t}" }
+                        else
+                          column_name
+                        end
       end
 
       def column_name
@@ -131,7 +131,7 @@ module Napa
       end
 
       def foreign_key?
-        !!(name =~ /_id$/)
+        !(name =~ /_id$/).nil?
       end
 
       def reference?
@@ -139,31 +139,27 @@ module Napa
       end
 
       def polymorphic?
-        self.attr_options.has_key?(:polymorphic)
+        attr_options.key?(:polymorphic)
       end
 
       def required?
-        self.attr_options[:required]
+        attr_options[:required]
       end
 
-      def has_index?
-        @has_index
-      end
-
-      def has_uniq_index?
-        @has_uniq_index
-      end
+      attr_reader :has_uniq_index, :has_index
+      alias_method :has_uniq_index?, :has_uniq_index
+      alias_method :has_index?, :has_index
 
       def password_digest?
         name == 'password' && type == :digest
       end
 
       def inject_options
-        "".tap { |s| @attr_options.each { |k,v| s << ", #{k}: #{v.inspect}" } }
+        ''.tap { |s| @attr_options.each { |k, v| s << ", #{k}: #{v.inspect}" } }
       end
 
       def inject_index_options
-        has_uniq_index? ? ", unique: true" : ""
+        has_uniq_index? ? ', unique: true' : ''
       end
     end
   end
