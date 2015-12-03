2ed813b5bf92b5c5964dc2a1d02d8c580d667881
Ashton Thomas <athomas@bellycard.com>
2015-12-03 13:24:27 -0600

    upgrade roar to current version - this is two years out of date - Removes Representer and Feature namespace - added more stuff for JSONAPI, hypermedia - This is going to bump the Representer gem which could cause issues for some project
    update our representer to work with various versions of roar
    
    allow for higher versions of roar
    
    prefer version check over load error
    
    pessimistic on upper version of roar
    
    avoid using a global constant to include JSON module

--- lib/napa/output_formatters/representer.rb
+++ lib/napa/output_formatters/representer.rb
@@ -1,11 +1,23 @@
 require 'roar/decorator'
-require 'roar/representer/json'
-require 'roar/representer/feature/coercion'
+require 'roar/version'
+
+if Roar::VERSION >= '1.0.0'
+  require 'roar/json'
+  require 'roar/coercion'
+else
+  require 'roar/representer/json'
+  require 'roar/representer/feature/coercion'
+end
 
 module Napa
   class Representer < Roar::Decorator
-    include Roar::Representer::JSON
-    include ::Representable::Coercion
+    include Coercion
+
+    if Roar::VERSION >= '1.0.0'
+      include Roar::JSON
+    else
+      include Roar::Representer::JSON
+    end
 
     property :object_type, getter: ->(*) { self.class.name.underscore }
   end
