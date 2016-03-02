fdd3f0ea8e37e392b18a2064daf670e9ece6d687
Nicholas Lee <nlee@bellycard.com>
2016-03-02 13:30:49 -0600

    no leading scope

--- lib/napa/logger/logging/logger.rb
+++ lib/napa/logger/logging/logger.rb
@@ -1,4 +1,4 @@
-class ::Logging::Logger
+class Logging::Logger
   def self.define_log_methods(logger)
     ::Logging::LEVELS.each do |name, num|
       code =  "undef :#{name}  if method_defined? :#{name}\n"
