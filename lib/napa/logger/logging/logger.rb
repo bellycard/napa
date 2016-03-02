d21799432ba3d9c1477da8346c9b63219181fabf
Nicholas Lee <nlee@bellycard.com>
2016-03-02 13:35:32 -0600

    make rubocop happy

--- lib/napa/logger/logging/logger.rb
+++ lib/napa/logger/logging/logger.rb
@@ -1,30 +1,32 @@
-class Logging::Logger
-  def self.define_log_methods(logger)
-    ::Logging::LEVELS.each do |name, num|
-      code =  "undef :#{name}  if method_defined? :#{name}\n"
-      code << "undef :#{name}? if method_defined? :#{name}?\n"
+module Logging
+  class Logger
+    def self.define_log_methods(logger)
+      ::Logging::LEVELS.each do |name, num|
+        code =  "undef :#{name}  if method_defined? :#{name}\n"
+        code << "undef :#{name}? if method_defined? :#{name}?\n"
 
-      if logger.level > num
-        code << <<-CODE
-          def #{name}?( ) false end
-          def #{name}( data = nil ) false end
-        CODE
-      else
-        code << <<-CODE
-          def #{name}?( ) true end
-          def #{name}( data = nil )
-            data = yield if block_given?
-            if data.kind_of?(Hash)
-              data = Napa::Logger.basic_request_format(data)
+        if logger.level > num
+          code << <<-CODE
+            def #{name}?( ) false end
+            def #{name}( data = nil ) false end
+          CODE
+        else
+          code << <<-CODE
+            def #{name}?( ) true end
+            def #{name}( data = nil )
+              data = yield if block_given?
+              if data.kind_of?(Hash)
+                data = Napa::Logger.basic_request_format(data)
+              end
+              log_event(::Logging::LogEvent.new(@name, #{num}, data, @caller_tracing))
+              true
             end
-            log_event(::Logging::LogEvent.new(@name, #{num}, data, @caller_tracing))
-            true
-          end
-        CODE
-      end
+          CODE
+        end
 
-      logger._meta_eval(code, __FILE__, __LINE__)
+        logger._meta_eval(code, __FILE__, __LINE__)
+      end
+      logger
     end
-    logger
   end
 end
