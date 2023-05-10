e90b3d1b37cf0ba6215ff357fbdb8098a488e353
Ryan Sims <ryan.sims@mobivity.com>
2023-05-10 14:57:10 -0700

    Got AASM working too

--- lib/napa/grape_extenders.rb
+++ lib/napa/grape_extenders.rb
@@ -3,28 +3,21 @@ module Napa
     def self.extended(modified_class)
       # when extended, set the exceptions to handle
       # if AR is being used, rescue from common AR errors
-      if defined?(::ActiveRecord)
-        modified_class.rescue_from :all do |e|
-          if e.class == ::ActiveRecord::RecordNotFound
-            err = Napa::JsonError.new(:record_not_found, 'record not found')
-            Napa::Logger.logger.debug(Napa::Logger.response(404, {}, err))
-            rack_response(err.to_json, 404)
-          elsif e.class == ::ActiveRecord::RecordInvalid
-            err = Napa::JsonError.new(:unprocessable_entity, e.message, e.record.errors.messages)
-            Napa::Logger.logger.debug(Napa::Logger.response(422, {}, err))
-            rack_response(err.to_json, 422)
-          else
-            raise e
-          end
-        end
-      end
-
-      # if AASM is being used, rescue from invalid transitions
-      if defined?(::AASM)
-        modified_class.rescue_from ::AASM::InvalidTransition do |e|
+      modified_class.rescue_from :all do |e|
+        if defined?(::ActiveRecord) and e.class == ::ActiveRecord::RecordNotFound
+          err = Napa::JsonError.new(:record_not_found, 'record not found')
+          Napa::Logger.logger.debug(Napa::Logger.response(404, {}, err))
+          rack_response(err.to_json, 404)
+        elsif defined?(::ActiveRecord) and e.class == ::ActiveRecord::RecordInvalid
+          err = Napa::JsonError.new(:unprocessable_entity, e.message, e.record.errors.messages)
+          Napa::Logger.logger.debug(Napa::Logger.response(422, {}, err))
+          rack_response(err.to_json, 422)
+        elsif defined?(::AASM) and e.class == ::AASM::InvalidTransition
           err = Napa::JsonError.new(:unprocessable_entity, e.message)
           Napa::Logger.logger.debug(Napa::Logger.response(422, {}, err))
           rack_response(err.to_json, 422)
+        else
+          raise e
         end
       end
     end
