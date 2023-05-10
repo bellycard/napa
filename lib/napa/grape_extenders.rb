04f306cc91e3be9fdbd919b9344f0690efd76a66
Ryan Sims <ryan.sims@mobivity.com>
2023-05-10 14:16:06 -0700

    Fix an issue where it appears the exceptions were not being handled
    Why this needed to change? I have no idea really, but it works to pass the tests so ¯\_(ツ)_/¯

--- lib/napa/grape_extenders.rb
+++ lib/napa/grape_extenders.rb
@@ -2,18 +2,20 @@ module Napa
   module GrapeExtenders
     def self.extended(modified_class)
       # when extended, set the exceptions to handle
-
       # if AR is being used, rescue from common AR errors
       if defined?(::ActiveRecord)
-        modified_class.rescue_from ::ActiveRecord::RecordNotFound do
-          err = Napa::JsonError.new(:record_not_found, 'record not found')
-          Napa::Logger.logger.debug(Napa::Logger.response(404, {}, err))
-          rack_response(err.to_json, 404)
-        end
-        modified_class.rescue_from ::ActiveRecord::RecordInvalid do |e|
-          err = Napa::JsonError.new(:unprocessable_entity, e.message, e.record.errors.messages)
-          Napa::Logger.logger.debug(Napa::Logger.response(422, {}, err))
-          rack_response(err.to_json, 422)
+        modified_class.rescue_from :all do |e|
+          if e.class == ::ActiveRecord::RecordNotFound
+            err = Napa::JsonError.new(:record_not_found, 'record not found')
+            Napa::Logger.logger.debug(Napa::Logger.response(404, {}, err))
+            rack_response(err.to_json, 404)
+          elsif e.class == ::ActiveRecord::RecordInvalid
+            err = Napa::JsonError.new(:unprocessable_entity, e.message, e.record.errors.messages)
+            Napa::Logger.logger.debug(Napa::Logger.response(422, {}, err))
+            rack_response(err.to_json, 422)
+          else
+            raise e
+          end
         end
       end
 
